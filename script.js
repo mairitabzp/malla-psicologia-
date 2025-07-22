7871abc1fc67c1162c5dff20e04c09a98a86542c
mairitabzp <mairitabzp@gmail.com>
2025-07-22 00:24:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+git clone https://github.com/TU_USUARIO/malla-psicologia-ucsh.git
+cd malla-psicologia-ucsh
+unzip malla_psicologia_ucsh_2025.zip
+git add .
+git commit -m "Subiendo malla 2025"
+git push origin main
 
